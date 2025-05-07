72541cd375060a1a51314fa10d509522567cf08d
Erico Djasmeini <ericodjasmeini@gic.com.sg>
2025-05-07 22:55:21 +0800

    feat: add loop through all files

--- anacondaworkshop01/main.py
+++ anacondaworkshop01/main.py
@@ -1,1 +1,7 @@
-print("abc")
+from util import Util
+import conf
+
+if __name__ == '__main__':
+    for date in conf.DATES:
+        for fund, pattern in conf.FUNDS.items():
+           print(fund + " " + pattern)
