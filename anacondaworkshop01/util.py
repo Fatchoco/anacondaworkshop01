8ed1152b4e6d5d0fbb1589ddd23aa82c9c086e0c
Erico Djasmeini <ericodjasmeini@gic.com.sg>
2025-05-07 23:10:32 +0800

    feat: add engine to duckdb

--- anacondaworkshop01/util.py
+++ anacondaworkshop01/util.py
@@ -1,7 +1,11 @@
+from typing import ClassVar
+
 from pydantic import BaseModel, ConfigDict
+from sqlalchemy import create_engine
 
 class Util(BaseModel):
     model_config = ConfigDict(validate_assignment=True)
+    engine: ClassVar = create_engine(r"duckdb:///../db/dbfile.db")
 
     @staticmethod
     def extract_load(fund_name: str, report_date: str, file_pattern: str) -> None:
