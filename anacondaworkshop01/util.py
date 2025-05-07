fb88fe41d0dbec402d2585a88c1c8ca335c70dce
Erico Djasmeini <ericodjasmeini@gic.com.sg>
2025-05-07 23:59:03 +0800

    feat: add test for extracting file

--- anacondaworkshop01/util.py
+++ anacondaworkshop01/util.py
@@ -10,6 +10,7 @@ from datetime import datetime
 class Util(BaseModel):
     model_config = ConfigDict(validate_assignment=True)
     engine: ClassVar = create_engine(r"duckdb:///" + conf.DUCKDB_FILEPATH)
+    input_path: ClassVar = conf.PATH_INPUT
 
     @staticmethod
     def load_all_files() -> None:
@@ -28,11 +29,16 @@ class Util(BaseModel):
     @staticmethod
     def _extract_and_load(file_pattern: str, fund_name: str, report_date: str) -> None:
         print(f"Processing: {fund_name} - {report_date} - {file_pattern}")
+        df = Util._extract_file(file_pattern, fund_name, report_date)
+        df.to_sql("raw_fund_eom_report", Util.engine, if_exists="append", index=False)
+
+    @staticmethod
+    def _extract_file(file_pattern, fund_name, report_date) -> pd.DataFrame:
         date_format = "%Y-%m-%d"
         date_obj = datetime.strptime(report_date, date_format)
         file_name = date_obj.strftime(file_pattern)
-        df = pd.read_csv(f"{conf.PATH_INPUT}{file_name}", dtype=str)
+        df = pd.read_csv(f"{Util.input_path}{file_name}", dtype=str)
         df["REPORT DATE"] = report_date
         df["FUND NAME"] = fund_name
         df["_file_name"] = file_name
-        df.to_sql("raw_fund_eom_report", Util.engine, if_exists="append", index=False)
+        return df
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,20 @@
 import pytest
 
-from anacondaworkshop01 import util
+from anacondaworkshop01.util import Util
 
 def test_true():
     assert True
+
+def test__extract_file(mocker):
+    report_date = "2023-02-28"
+    fund_name = "Testfund"
+    file_pattern = "Testfund.%d-%m-%Y breakdown.csv"
+    mocker.patch("anacondaworkshop01.util.Util.input_path", 'tests/files/')
+
+    df = Util._extract_file(file_pattern, fund_name, report_date)
+
+    assert 'REPORT DATE' in df.columns
+    assert 'FUND NAME' in df.columns
+    assert '_file_name' in df.columns
+    assert df['REPORT DATE'].iloc[0] == report_date
+    assert df['FUND NAME'].iloc[0] == fund_name
